9d10e0dfb0506c1a443fa2fe1117d40be23c4f6c
returnordie <antonsteinn@icloud.com>
2026-01-15 19:57:43 +0000

    Inline modal components

--- resources/js/Pages/Profile/Partials/DeleteUserForm.tsx
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.tsx
@@ -1,11 +1,90 @@
 import DangerButton from '@/Components/DangerButton';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
-import Modal from '@/Components/Modal';
 import SecondaryButton from '@/Components/SecondaryButton';
 import TextInput from '@/Components/TextInput';
+import {
+    Dialog,
+    DialogPanel,
+    Transition,
+    TransitionChild,
+} from '@headlessui/react';
 import { useForm } from '@inertiajs/react';
-import { FormEventHandler, useRef, useState } from 'react';
+import {
+    FormEventHandler,
+    PropsWithChildren,
+    RefObject,
+    useRef,
+    useState,
+} from 'react';
+
+function Modal({
+    children,
+    show = false,
+    maxWidth = '2xl',
+    closeable = true,
+    initialFocus,
+    onClose = () => {},
+}: PropsWithChildren<{
+    show: boolean;
+    maxWidth?: 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+    closeable?: boolean;
+    initialFocus?: RefObject<HTMLElement>;
+    onClose: CallableFunction;
+}>) {
+    const close = () => {
+        if (closeable) {
+            onClose();
+        }
+    };
+
+    const maxWidthClass = {
+        sm: 'sm:max-w-sm',
+        md: 'sm:max-w-md',
+        lg: 'sm:max-w-lg',
+        xl: 'sm:max-w-xl',
+        '2xl': 'sm:max-w-2xl',
+    }[maxWidth];
+
+    return (
+        <Transition show={show} leave="duration-200">
+            <Dialog
+                as="div"
+                id="modal"
+                open={show}
+                className="fixed inset-0 z-50 flex transform items-center overflow-y-auto px-4 py-6 transition-all sm:px-0"
+                onClose={close}
+                initialFocus={initialFocus}
+            >
+                <TransitionChild
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="absolute inset-0 bg-gray-500/75" />
+                </TransitionChild>
+
+                <TransitionChild
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enterTo="opacity-100 translate-y-0 sm:scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                    <DialogPanel
+                        className={`mb-6 transform overflow-hidden rounded-lg bg-white shadow-xl transition-all sm:mx-auto sm:w-full ${maxWidthClass}`}
+                    >
+                        {children}
+                    </DialogPanel>
+                </TransitionChild>
+            </Dialog>
+        </Transition>
+    );
+}
 
 export default function DeleteUserForm({
     className = '',
