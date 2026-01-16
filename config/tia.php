613588a64993b7d32c112d3c794b31d589f6175d
Anton <antonsteinn@icloud.com>
2026-01-16 10:55:04 +0000

    Added buttons styles

--- config/tia.php
+++ config/tia.php
@@ -3,7 +3,7 @@
 return [
     'username_change_days' => 30,
     'ad_default_duration_days' => 14,
-    'ad_extend_allowed_days' => [14, 21, 30],
+    'ad_extend_allowed_days' => [14],
     'ad_extend_max_days_ahead' => 180,
     'notifications_prune_days' => 10,
     'deal_review_delay_hours' => 0,
--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,6 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import '../css/app.css';
 import './bootstrap';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 import { createInertiaApp } from '@inertiajs/react';
